53f1310c734afc913ce20d3e89c140a1074aa8e6
Alex Singleton <alexanderjsingleton@alexs-imac.cisco>
2017-09-13 12:33:16 -0500

    first iteration complete

--- shopping_list.py
+++ shopping_list.py
@@ -2,19 +2,23 @@
 shopping_list = []
 
 # print out instructions on how to use the app
-print("What should we pick up at the store?")
-print("Enter 'DONE' to stop adding items.")
+print("Whathu gonna have at the chopshop?")
+print("Enter 'CHOPPED' to stop adding items.")
 
 while True:
 	# ask for new items
 	new_item = input("> ")
 
-	if new_item == 'DONE':
-		break
-
-
 	# add new ites to our list
 	shopping_list.append(new_item)
 
+	if new_item == 'DONE':
+		break
+
 # be able to quit the app
-# print out the listls
+
+# print out the list
+print("Here's your list:")
+
+for item in shopping_list:
+	print(item)
